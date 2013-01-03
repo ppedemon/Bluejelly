e68d166726dbf1f05a417dfe8a456f28fa3c57b5
Pablo Pedemonte <ppedemonte@gmail.com>
2013-01-03 18:12:53 -0300

    Finally, a serious test suite for the Bluejelly Assembler

--- Bluejelly.Asm/test/bluejelly/asm/test/AsmTest.scala
+++ Bluejelly.Asm/test/bluejelly/asm/test/AsmTest.scala
@@ -4,58 +4,29 @@
  * This source code is distributed under the terms of 
  * the BSD license, see the LICENSE file for details.
  */
-
 package bluejelly.asm.test
 
-import java.io.File
-
-import scala.Array.canBuildFrom
-
-import bluejelly.asm.AsmConfig
-import bluejelly.asm.Assembler
 import junit.framework.TestCase
+import junit.framework.Assert
 
 /**
- * Very simple testing of the Bluejelly asembler. Ideally, we should
- * run the generated class using the runtime, redirectng stderr and
- * stdout to some file or stream and checking for expected results.
- * 
- * <p>Disclaimer: unfortunately, at the moment I don't have the
- * time to implement tests in the way describe above. Maybe later.
- * For now, just check that we are generating a class when we should.
- * 
- * <p>Assumption: all modules to test are located in the <code>mods</code>
- * folder, and module name is of the form <code>asm.X</code>.
- * 
+ * Common code for all assembler tests.
  * @author ppedemon
  */
 class AsmTest extends TestCase {
   
-  def testValidModules() {
-    assemble(validMods)
-    new File("mods/asm").listFiles foreach (_ exists)
+  private val asmRunner = new AsmRunner
+  
+  protected def check(mod:String, cmd:String, expected:String) {
+    val out = asmRunner.run(mod, cmd)
+    println(out)
+    Assert.assertEquals(out, expected)
   }
-  
-  def testInvalidModules() {
-      assemble(invalidMods)
-      invalidMods map classFile forall (f => !(f exists))    
+
+  protected def check(mod:String, cmds:Seq[(String,String)]) {
+    val (cs,es) = cmds unzip
+    val out = asmRunner.run(mod, cs)
+    println(out)
+    Assert.assertEquals(out, es)
   }
-  
-  private def modsWhere(p:String => Boolean):Array[String] = {
-    val base = new File("mods")
-    base.list filter (n => p(n) && (n endsWith ".jas")) map ("mods/" + _)
-  }
-  
-  private def validMods = modsWhere(n => !(n startsWith "Fail_"))
-  private def invalidMods = modsWhere(_ startsWith "Fail_")
-  
-  private def classFile(modName:String) = 
-    new File(("mods/asm/" + modName.substring(5)) replaceFirst ("jas$", "class"))
-  
-  private def assemble(modules:Array[String]) {
-    val cfg = new AsmConfig
-    cfg.outDir = "mods"
-    modules foreach (Assembler.assemble(cfg,_))
-  }
-  
 }
---
+++ Bluejelly.Asm/test/bluejelly/asm/test/ExtrasTest.scala
@@ -0,0 +1,60 @@
+/*
+ * The Bluejelly project, Copyright 2012.
+ *
+ * This source code is distributed under the terms of 
+ * the BSD license, see the LICENSE file for details.
+ */
+package bluejelly.asm.test
+
+import scala.collection.mutable.MutableList
+import scala.math.BigInt
+
+import org.junit.Test
+
+/**
+ * Extras tests: test "real-world" code in modules:
+ *   Nest.jas, List.jas, Primes.jas and Fib.jas
+ *   
+ * @author ppedemon
+ *
+ */
+class ExtrasTest extends AsmTest {
+
+  // For testing Primes.primesList
+  private def sieve(max:Int):Array[Int] = {
+    val a = (2 to max map {(true,_)}).toArray
+    for (i <- 0 until a.length)
+      if (a(i)._1)
+        for (j <- i + a(i)._2 until a.length by a(i)._2)
+          a(j) = (false,a(i)._2)
+    a filter {_._1} map (_._2)
+  }
+
+  // For testing Fib.fibList
+  private def fib(n:Int):MutableList[BigInt] = {
+    val xs = MutableList[BigInt](0,1)
+    for (i <- 2 until n) xs += xs(i-2) + xs(i-1)
+    xs
+  }
+  
+  @Test def testNestedEval() {
+    check("Nest.jas", "Nest.main", 7 toString)
+  }
+  
+  @Test def testListFuns() {
+    check("List.jas", Seq(
+      (":List.testEnumFromInt", 1 to 10 mkString ("[",",","]")),
+      (":List.filterTest", 1 to 10 filter {_ % 2 == 0} mkString ("[",",","]"))
+    ))
+  }
+    
+  @Test def testPrims() {
+    val ps = sieve(250) take 50 mkString ("[",",","]")
+    check("Primes.jas", ":Primes.primesList", ps)
+  }
+  
+  @Test def testFib() {
+    val xs = fib(100) mkString ("[",",","]")
+    check("Fib.jas", ":Fib.fibList", xs)
+  }
+}
