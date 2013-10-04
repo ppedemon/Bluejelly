1a1977e689d027fd076adc7516c54389aeabf3b9
Pablo Pedemonte <ppedemonte@gmail.com>
2013-10-04 20:37:34 -0300

    Interpolable strings. As yet unused, but might be handy at some point.

---
+++ bluejelly-utils/src/main/scala/bluejelly/utils/InterpolableString.scala
@@ -0,0 +1,30 @@
+/*
+ * The Bluejelly project, Copyright 2012.
+ *
+ * This source code is distributed under the terms of 
+ * the BSD license, see the LICENSE file for details.
+ */
+package bluejelly.utils
+
+import scala.util.matching.Regex
+
+/**
+ * Strings supporting interpolation, as implemented here:
+ * 
+ *   http://dcsobral.blogspot.com.ar/2010/01/string-interpolation-in-scala-with.html
+ *   
+ * @author ppedemon
+ */
+object InterpolableString {
+  implicit def strToInterpolableStr(s:String) = new {
+    def fill(m:Map[String,String]):String = """\$\{([^}]+)\}""".r.replaceAllIn(s, 
+        (_:Regex.Match) match {
+          case Regex.Groups(v) => m.getOrElse(v, "")
+        })
+    def fill(ps:Pair[String,String]*):String = {
+      val s = fill(Map(ps:_*))
+      println("*** " + s)
+      s
+    }
+  }
+}
